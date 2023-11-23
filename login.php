2ec0d2147cac291336226dbb523a5a9c25003585
Joahan <joahan.cervantes5043@alumnos.udg.mx>
2023-11-22 19:24:29 -0600

    Actualicé el formato de la pestaña de login para realizar la verificación

--- login.php
+++ login.php
@@ -6,33 +6,70 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@500&display=swap" rel="stylesheet">
+ 
+  <script src="./js/jquery-3.3.1.min.js"></script>
+    <script>
+        function recibe(){
+            var correo = $("#correo").val();
+            var pass = $("#pasw").val();
+                $.ajax({
+                    url: './funciones/checkUser.php',
+                    method: "POST", 
+                    dataType: 'text',
+                    data: {correo:correo, pass:pass},
+                    success: function(res){
+                            if(res==0){
+                                $('#checkCampos').html('Usuario no valido');
+                                return false;
+                            }
+                            else{
+                               return true;
+                            }  
+                    
+                    }
+                });
+            }
+        }
+
+    </script>
+
+
 </head>
 <body>
-    <header>
+    <header >
         <div class="container-header">
-            <h1><img src="img/Logo.png" alt="logo"></h1>
+            <h1> 
+                <a href="landingPage.html">
+                    <img src="img/Logo.png" alt="logo">
+                </a>
+            </h1>
+
+            <nav class="landing-nav">
+            </nav>
         </div>
     </header>
-<div class="principal">
-    <div class="main-login" style="border: 1px black solid;">
-        <div class="inicia-sesion">
-            <h1><span style="color: #0DCEDA; padding-left: 50px;">INICIA&nbsp;</span>SESIÓN</h1>
-            <h2>Por favor llena el formulario para crear tu cuenta</h2>
-        </div>
 
-        <div class="cuadro-R">
-            <h4>Nombre de usuario</h4>
-            <input type="text" placeholder="Nombre de usuario">
-            <h4>Correo electronico</h4>
-            <input type="text" placeholder="Correo electronico">
-            <h4>Contraseña</h4>
-            <input type="text" placeholder="Contraseña">
-            <button type="button">Continuar</button>
-            <div class="lineas-y-o">
-            <hr><span>&nbsp;O&nbsp;</span><hr>
+    <div class="titulo-L">
+        <h1><span style="color: #0DCEDA;">INICIA&nbsp;</span>SESIÓN</h1>
+        <h2>Por favor llena el formulario para acceder a tu cuenta</h2>
+    </div>
+<form onsubmit="return recibe();" name="login" id="login" method="POST" action="./funciones/Validacion.php">
+    <div class="cuadro-L">
+        <h4>Correo electrónico</h4>
+        <input type="email" id="correo" name="correo" placeholder="Escribe tu correo..." required>
+        <h4>Contraseña</h4>
+        <input type="password" name="pasw" id="pasw" placeholder="Escribe tu contraseña..." required>
+        <div class="checkCampos" id="checkCampos">...</div>
+        <a class="forgot-pass">¿Olvidaste tu contraseña?</a>
+        <button type="submit" class="boton">Acceder</button>
+<form>
+        <div class="signup">
+            ¿No tienes una cuenta?  
+            <a href=registrousuario.html>Crea una</a>
         </div>
-        <button class="button-Google">Acceder con Google</button>
-        </div>
-</div>
+        <a class="guest">Acceder como invitado</a>
+    </div>
+
+    <img class="img-L" src="img/2GATITOS.png">
 </body>
 </html>
