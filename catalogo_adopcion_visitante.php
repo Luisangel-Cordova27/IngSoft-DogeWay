b670234f6e2f562f41da1e74c5f12c5253e92b11
crisanyel <luisangel.cordova6357@alumnos.udg.mx>
2023-11-28 16:41:59 -0600

    arregle unos pedillos del pull

--- catalogo_adopcion_visitante.php
+++ catalogo_adopcion_visitante.php
@@ -68,7 +68,7 @@
                                 '.$user.'</p>
                             </div>';
                             ?>
-                           <a href="detalleMascota.php"><img src="./img_mascotas/<?php echo $foto ?>" style="height: 175px; width: 250px; align-items:center;"/></a>
+                            <img src="./img_mascotas/<?php echo $foto ?>" style="height: 175px; width: 250px; align-items:center;">
                             <?php
                             echo '<p class="nombreAnimal" style="font-size:15px;" align="left">'.$nombre.'</p>';
                             echo '<p class="razaAnimal" style="font-size:15px; float:left; color: gray;">'.$raza.'</p>';
