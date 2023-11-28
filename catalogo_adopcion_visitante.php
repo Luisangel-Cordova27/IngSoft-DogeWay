aecf8691fe9705ac219dafb89c8c8ae86c474efa
crisanyel <luisangel.cordova6357@alumnos.udg.mx>
2023-11-28 16:58:14 -0600

    Agregué animación a la hora de poner el mouse encima de las tarjetas del catalogo de adopcion

--- catalogo_adopcion_visitante.php
+++ catalogo_adopcion_visitante.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta charset="UTF-8">
-  <link rel="stylesheet" href="style.css?v=1.1">
+  <link rel="stylesheet" href="style.css?v=1.2">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap" rel="stylesheet">
