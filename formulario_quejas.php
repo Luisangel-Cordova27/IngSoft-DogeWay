9a345159ae9eddc2651ace6452e95dfbb1422be9
val <valeria.ceja4953@alumnos.udg.mx>
2023-11-28 20:06:47 -0600

    Actualización de formulario para quejas de usuarios.

--- formulario_quejas.php
+++ formulario_quejas.php
@@ -14,34 +14,32 @@ if ($conn->connect_error) {
 
 // Recibe los datos del formulario
 $fechaIncidente = isset($_POST['fechaIncidente']) ? $_POST['fechaIncidente'] : '';
-$usuarioReportado = isset($_POST['usuarioReport']) ? $_POST['usuarioReport'] : '';
-$nombreUsuario = isset($_POST['nombreUsuario']) ? $_POST['nombreUsuario'] : '';
-$apellidoUsuario = isset($_POST['apellidoUsuario']) ? $_POST['apellidoUsuario'] : '';
+$usuarioReportado = isset($_POST['usuarioReportado']) ? $_POST['usuarioReportado'] : '';
+$usuarioQueReporta = isset($_POST['usuarioQueReporta']) ? $_POST['usuarioQueReporta'] : '';
 $ubicacionIncidente = isset($_POST['ubicacionIncidente']) ? $_POST['ubicacionIncidente'] : '';
 $detallesIncidente = isset($_POST['detallesIncidente']) ? $_POST['detallesIncidente'] : '';
 
 // Verifica los datos antes de insertarlos
-if (!empty($fechaIncidente) && !empty($usuarioReportado) && !empty($nombreUsuario) && !empty($apellidoUsuario) && !empty($ubicacionIncidente) && !empty($detallesIncidente)) {
+if (!empty($fechaIncidente) && !empty($usuarioReportado) && !empty($usuarioQueReporta) &&  !empty($ubicacionIncidente) && !empty($detallesIncidente)) {
 
     $fechaIncidente = date('Y-m-d', strtotime($fechaIncidente));
 
     // Realiza la inserción en la base de datos
-    $sql = "INSERT INTO formulario_quejas (fecha_incidente, usuario_reportado, nombre_usuario, apellido_usuario, ubicacion_incidente, detalles_incidente) 
-            VALUES ('$fechaIncidente', '$usuarioReportado', '$nombreUsuario', '$apellidoUsuario', '$ubicacionIncidente', '$detallesIncidente')";
+    $sql = "INSERT INTO formulario_quejas (fecha_incidente, id_usuario_reportado, id_usuario_quereporta, ubicacion_incidente, detalles_incidente) 
+            VALUES ('$fechaIncidente', '$usuarioReportado', '$usuarioQueReporta', '$ubicacionIncidente', '$detallesIncidente')";
 
     if ($conn->query($sql) === TRUE) {
         
-        echo "Formulario enviado correctamente";
+        echo '<script type="text/javascript">window.alert("Formulario enviado correctamente :)");</script>'  ;
         
     } else {
         
-        echo "Error al enviar el formulario: " . $conn->error;
+        echo '<script type="text/javascript">window.alert("Error de envío. Inténtalo nuevamente :)");</script>';
+
     }
 } else {
     
-    echo "Error: Datos incompletos";
+    echo '<script type="text/javascript">window.alert("Error: Datos incompletos.");</script>';
+
+
 }
-
-
-$conn->close();
-?>
